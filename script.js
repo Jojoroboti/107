0bcd93f746cdfefb80a3551c11414164c441fb96
Jojoroboti <jonathan@roeseonline.de>
2025-05-03 19:46:57 +0200

    changes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 (async function(){
-  const params = new URLSearchParams(window.location.search);
-  const key    = params.get('t');
-  if (!key) {
-    document.body.textContent = 'Kein Zielparameter angegeben.';
-    return;
-  }
-
-  try {
-    const url  = `https://raw.githubusercontent.com/Jojoroboti/107/main/mapping.json?t=${Date.now()}`;
-    const resp = await fetch(url, { cache: 'no-store' });
-    if (!resp.ok) throw new Error(`Fetch-Fehler: ${resp.status}`);
-    const mapping = await resp.json();
-
-    const dest = mapping[key];
-    if (dest) {
-    //   document.body.textContent = 'Weiterleitung …';
-      window.location.replace(dest);
-    } else {
-      document.body.textContent = `Ziel „${key}“ nicht gefunden.`;
+    const params = new URLSearchParams(window.location.search);
+    const key    = params.get('t');
+    if (!key) {
+            document.getElementById("infotext").textContent = 'There was no target provided.';
+            return;
     }
-  } catch (e) {
-    console.error('Redirect-Fehler:', e);
-    document.body.textContent = 'Ein Fehler ist aufgetreten. Bitte später erneut versuchen.';
-  }
+
+    try {
+        const url  = `https://raw.githubusercontent.com/Jojoroboti/107/main/mapping.json?t=${Date.now()}`;
+        const resp = await fetch(url, { cache: 'no-store' });
+        if (!resp.ok) throw new Error(`Fetch-Fehler: ${resp.status}`);
+        const mapping = await resp.json();
+
+        const dest = mapping[key];
+        if (dest) {
+            document.body.textContent = 'Joining...';
+            window.location.replace(dest);
+        } else {
+            document.getElementById("infotext").textContent = `Target „${key}“ not found.`;
+        }
+    } catch (e) {
+        console.error('Redirect-Fehler:', e);
+        document.getElementById("infotext").textContent = 'An unknown error occured. Please try again later.';
+    }
 })();
