2742e7891e4414407381eaf752cf7b43109134de
Jojoroboti <jonathan@roeseonline.de>
2025-06-10 15:11:48 +0200

    a

--- join/script.js
+++ join/script.js
@@ -2,6 +2,10 @@
     const infotext = document.getElementById("infotext");
     const params = new URL(window.location).searchParams;
     const key = params.get("b") || params.get("j");
+    if (params.get("f")) {
+        window.location.replace(`roblox://experiences/start?placeId=4639625707&gameInstanceId=${params.get("f")}`)
+    }
+
     if (!key) {
         infotext.textContent = "There was no target provided.";
         return;
