96d9586238a67241c24b5e76574d331e528976e2
Jojoroboti <jonathan@roeseonline.de>
2025-09-13 17:31:11 +0200

    Update script.js

--- join/script.js
+++ join/script.js
@@ -2,8 +2,7 @@
     const subtitle = document.getElementById("subtitle");
     const params = new URLSearchParams(window.location.search);
     if (params.get("f")) {
-        window.location.replace(`roblox://experiences/start?placeId=4639625707&gameInstanceId=${params.get("f")}`);
+        window.location.replace(`roblox://experiences/start?placeId=4639625707&gameInstanceId=${params.get("f")}`)
         subtitle.textContent = "Joining...";
     }
 )();
-
