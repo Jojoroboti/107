7e1d3fd8efc742bb38f3c8e846c256c49e4c9ca9
Jojoroboti <jonathan@roeseonline.de>
2025-09-13 17:08:03 +0200

    Update script.js

--- join/script.js
+++ join/script.js
@@ -1,8 +1,8 @@
 (async () => {
     const infotext = document.getElementById("infotext");
     const params = new URLSearchParams(window.location.search);
-    const key = params.get("b") || params.get("j");
     if (params.get("f")) {
-        window.location.replace(`roblox://experiences/start?placeId=4639625707&gameInstanceId=${params.get("f")}`)
+        window.location.replace(`roblox://experiences/start?placeId=4639625707&gameInstanceId=${params.get("f")}`);
+        infotext.textContent = "Joining...";
     }
 )();
